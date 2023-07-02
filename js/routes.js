efc38dfd40259e8e54e3ac9bdfe1eb2926a07dbe
Drvgged <lucionicolaslopez07@gmail.com>
2023-07-02 15:35:53 -0300

    v1.6

--- js/routes.js
+++ js/routes.js
@@ -6,12 +6,12 @@ const PATHS = {
     about: {
         path: "/about",
         template: `
-    <section id="about">
+    <section id="aboutus" class="aboutus">
         <h1>Welcome to Couplex Co.</h1>
         <p>At Couplex, we specialize in creating stunning websites that engage, inspire, and deliver results. With our team of experienced developers and designers, we bring your ideas to life and help your business thrive online.</p>
     </section>
 
-    <section id="whychu">
+    <section id="whychu" class="aboutus">
         <h1>Why Choose Us?</h1>
         <ul>
             <li>Tailored Solutions: We understand that each business is unique. Our solutions are customized to meet your specific needs, ensuring a website that aligns perfectly with your brand and goals.</li>
@@ -26,7 +26,7 @@ const PATHS = {
         template: `<h1>Contact</h1>`,
     },
     services: {
-        path: "/contact",
+        path: "/services",
         template: `<h1>Services</h1>`,
     },
 }
